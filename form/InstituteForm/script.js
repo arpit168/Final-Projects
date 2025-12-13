0db9d481b5bc94c8a5bac29532d65d291261f3dd
arpit168 <arpitgupta0406@gmail.com>
2025-12-13 19:50:03 +0530

    Refactor form submission logic to enhance validation and user feedback

--- form/InstituteForm/script.js
+++ form/InstituteForm/script.js
@@ -1,4 +1,6 @@
-function Submit() {
+function Submit(event) {
+  event.preventDefault();
+
   const fullName = document.getElementById("fullName").value.trim();
   const gender = document.getElementById("gender").value;
   const dob = document.getElementById("dob").value;
@@ -16,35 +18,75 @@ function Submit() {
   const guardianFullName = document
     .getElementById("guardianFullName")
     .value.trim();
-    const gurRelation = document.getElementById("gurRelation").value.trim();
+  const gurRelation = document.getElementById("gurRelation").value.trim();
   const gurContact = document.getElementById("gurContact").value.trim();
   const additional = document.getElementById("additional").value.trim();
 
-  if (fullName === "") {
-    alert("Please enter full name.");
+  // Full name
+
+  if (!fullName) {
+    document.getElementById("NameError").innerText = "Required";
+    return;
+  } else if (!/^[A-Za-z ]+$/.test(fullName)) {
+    document.getElementById("NameError").innerText =
+      "Only Alphabets and Spaces are Allowed";
     return;
   }
+
+  // Gender
 
   if (gender === "") {
     alert("Please select gender");
     return;
   }
-  if (dob === "") {
-    alert("Please enter date of birth.");
+
+  // ḌOB
+
+  if (!dob) {
+    document.getElementById("NameError").innerText = "Required";
+    return;
+  } else {
+    const currentyear = new Date().getFullYear();
+    const birthyear = Number(dob.split("-")[0]);
+    if (currentyear - birthyear < 18) {
+      document.getElementById("DOBError").innerText =
+        "You must be 18 years Old";
+      return;
+    }
+  }
+
+  // Phone
+
+  if (!phone) {
+    document.getElementById("PhoneError").innerText = "Required";
+    return;
+  } else if (!/^[6-9]\d{9}$/.test(phone)) {
+    document.getElementById("PhoneError").innerText =
+      "Only Indian Mobile Nummber allowed";
     return;
   }
-  if (!/^[0-9]{10}$/.test(phone)) {
-    alert("Please enter a vailid 10 digit mobile number");
+
+  // Email
+
+  if (!email) {
+    document.getElementById("EmailError").innerText = "Required";
+    return;
+  } else if (
+    !/^[\w.]+@(gmail|outlook|rediff|yahoo)\.(com|in|co\.in)$/.test(email)
+  ) {
+    document.getElementById("EmailError").innerText = "Use Proper Email Format";
     return;
   }
-  if (!email.includes("@") || !email.includes(".")) {
-    alert("Please enter a vailid email");
-    return;
-  }
+
+  // Address
+
   if (fullAddress === "" || city === "" || state === "" || pinCode === "") {
     alert("Please complete address details");
     return;
   }
+
+  // guradian
+
   if (guardianFullName === "" || gurRelation === "" || gurContact === "") {
     alert("Please complete guardian details");
     return;
@@ -65,13 +107,12 @@ function Submit() {
     city,
     state,
     pinCode,
-    guradianFullName,
+    guardianFullName,
     gurRelation,
     gurContact,
     additional,
   };
- alert("Form Submitted Successfully!");
-alert("🎉Form submitted successfully!")
-console.log(formData);
 
+  alert("🎉Form submitted successfully!");
+  console.log(formData);
 }
