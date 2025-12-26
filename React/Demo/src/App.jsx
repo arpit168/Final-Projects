a4aa28a84cd194c170bc2582f3106465c03f8e12
arpit168 <arpitgupta0406@gmail.com>
2025-12-26 19:26:27 +0530

    in progress

--- React/Demo/src/App.jsx
+++ React/Demo/src/App.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 function App() {
-  let a =5 ;
-  let b= 10;
-  console.log(a+b);
+  let a = 5;
+  let b = 10;
+  console.log(a + b);
   return (
-    <> 
-  <h1 id='head'>Hello World</h1>
-  <div>This is My first react</div>
+    <>
+      <h1 id="head">Hello World <i className="bi bi-1-square"></i></h1>
+      <div>This is My first react</div>
     </>
-  )
-  
+  );
 }
 
 export default App;
