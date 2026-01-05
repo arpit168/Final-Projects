cfae60c6a3cdc36d716a64f36cf3a90921b1a43b
arpit168 <arpitgupta0406@gmail.com>
2026-01-06 01:45:59 +0530

    improvement

--- React/Makeup.com/src/App.jsx
+++ React/Makeup.com/src/App.jsx
@@ -1,38 +1,38 @@
-import React, { useEffect } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Header from "./component/Header";
-import Footer from "./component/Footer";
-import Home from "../src/Pages/Home";
-import About from "../src/Pages/About";
-import Product from "../src/Pages/Product";
-import Contact from "../src/Pages/Contact";
-import AOS from "aos";
-import Login from "./Pages/Login";
-import Signup from "./Pages/Signup";
-import {Toaster} from "react-hot-toast"
+// import React, { useEffect } from "react";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import Header from "./component/Header";
+// import Footer from "./component/Footer";
+// import Home from "../src/Pages/Home";
+// import About from "../src/Pages/About";
+// import Product from "../src/Pages/Product";
+// import Contact from "../src/Pages/Contact";
+// import AOS from "aos";
+// import Login from "./Pages/Login";
+// import Signup from "./Pages/Signup";
+// import {Toaster} from "react-hot-toast"
 
-const App = () => {
-  useEffect(() => {
-    AOS.init({ duration: 1000, once: false });
-  }, []);
-  return (
-    <>
-      <BrowserRouter>
+// const App = () => {
+//   useEffect(() => {
+//     AOS.init({ duration: 1000, once: false });
+//   }, []);
+//   return (
+//     <>
+//       <BrowserRouter>
       
-      <Toaster position="top-center" reverseOrder={false}/>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/product" element={<Product />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup/>} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-    </>
-  );
-};
+//       <Toaster position="top-center" reverseOrder={false}/>
+//         <Header />
+//         <Routes>
+//           <Route path="/" element={<Home />} />
+//           <Route path="/about" element={<About />} />
+//           <Route path="/product" element={<Product />} />
+//           <Route path="/contact" element={<Contact />} />
+//           <Route path="/login" element={<Login />} />
+//           <Route path="/signup" element={<Signup/>} />
+//         </Routes>
+//         <Footer />
+//       </BrowserRouter>
+//     </>
+//   );
+// };
 
-export default App;
+// export default App;
