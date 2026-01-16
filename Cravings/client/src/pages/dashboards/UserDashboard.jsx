88b7c77fc91e24b230f260a0358e459e15ee3493
arpit168 <arpitgupta0406@gmail.com>
2026-01-16 20:27:26 +0530

    add user dashboard components and routing

--- Cravings/client/src/pages/dashboards/UserDashboard.jsx
+++ Cravings/client/src/pages/dashboards/UserDashboard.jsx
@@ -5,15 +5,16 @@ import UserProfile from "../../components/userDashboard/userProfile";
 import UserOrders from "../../components/userDashboard/UserOrders";
 import UserTransaction from "../../components/userDashboard/UserTransaction";
 import UserHelpDesk from "../../components/userDashboard/UserHelpDesk";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const UserDashboard = () => {
   const [active, setActive] = useState("overview");
   return (
     <>
       <div className="w-full h-[90vh] flex ">
-        <div className=" w-3/14 bg-(--color-background)">
-          {" "}
-          <UserSidebar active={active} setActive={setActive} />
+        <div className=" w-3/14 bg-(--color-background) ">
+         
+         <GiHamburgerMenu/> <UserSidebar active={active} setActive={setActive} />
         </div>
         <div className="b w-11/14">
         {active === 'overview' && <UserOverview/>}
