4dfab3f4f75f18dc27623d2454ff103971de4e22
arpit168 <arpitgupta0406@gmail.com>
2026-01-27 19:21:20 +0530

    refactor: clean up code formatting and improve error logging

--- Cravings/server/index.js
+++ Cravings/server/index.js
@@ -1,24 +1,23 @@
-
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import cloudinary from "./src/config/cloudnary.js";
 import cookieParser from "cookie-parser";
 import connectDB from "./src/config/db.js";
 import AuthRouter from "./src/routers/authRouter.js";
 import publicRouter from "./src/routers/publicRouter.js";
-import userRouter from "./src/routers/userRouter.js"
-
+import userRouter from "./src/routers/userRouter.js";
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173", credentials:true}));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(morgan("dev"));
 
 app.use("/auth", AuthRouter);
-app.use("/public", publicRouter );
-app.use("/user",userRouter);
+app.use("/public", publicRouter);
+app.use("/user", userRouter);
 
 app.get("/", (req, res) => {
   console.log("Server is Working");
@@ -27,15 +26,20 @@ app.get("/", (req, res) => {
 app.use((err, req, res, next) => {
   const ErrorMessage = err.message || "Internal Server Error";
   const StatusCode = err.statusCode || 500;
-  console.log("Error Found",{ ErrorMessage, StatusCode});
-  
+  console.log("Error Found", { ErrorMessage, StatusCode });
 
   res.status(StatusCode).json({ message: ErrorMessage });
 });
 
 const port = process.env.port || 5000;
-app.listen(port, () => {
+app.listen(port, async () => {
   console.log("Server started at port:", port);
   connectDB();
-});
+  try {
+    const res = await cloudinary.api.ping();
 
+
+  } catch (error) {
+    
+  }
+});
